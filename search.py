4f060ec91db4a3e33a0c1288d89df1b39c8a38bc
ChairMane <danielschris899>
2019-12-09 19:38:08 -0800

    Finished output

--- search.py
+++ search.py
@@ -25,8 +25,6 @@ class featureSearch:
     def backward_feature_search(self):
         Y = np.copy(self.data[:, 0])    # Classifications
         X = np.delete(self.data, 0, 1)  # Features
-        #Y = np.array([1, 0, 1, 1, 0, 1, 0, 1, 0, 0])
-        #X = np.array([[2.7, 5.5], [8, 9.1], [0.9, 4.7], [1.1, 3.2], [5.4, 8.5], [2.9, 1.9], [6.1, 6.6], [0.5, 1], [8.3, 6.6], [8.1, 4.7]])
         (m, n) = X.shape
         current_set = {s for s in range(n)}
         accuracies = []
@@ -45,7 +43,7 @@ class featureSearch:
                         current_copy = set()
                         current_copy = current_set.copy()
                         current_copy.remove(k)
-                        #print('--Considering removing feature {}'.format(k+1))
+                        print('--Considering removing feature {}'.format(k+1))
                         accuracy = self.leave_one_out_CV(X, Y, current_copy, None, m)
 
                         if accuracy > best:
@@ -81,58 +79,58 @@ class featureSearch:
         else:
             correspond[frozenset()] = (m - default_rate) / m
             accuracies.append((0, (m-default_rate)/m))
+        print('When no elements exist in the set, the accuracy is: {:.1f}%\n\n'.format(accuracies[0][1]*100))
+        print('Beginning search. . .\n')
         for i in range(n):
-            print('On level {} of the search tree.'.format(i+1))
             feature_to_add = -1
             best = 0
             for k in range(n):
                 if k not in current_set:
-                    #print('--Considering adding feature {}'.format(k+1))
                     accuracy = self.leave_one_out_CV(X, Y, current_set, k, m)
-
+                    if len(current_set) == 0:
+                        print('\t\tUsing feature(s) {{{}}}: accuracy is {:.1f}%'.format(k+1, accuracy*100))
+                    else:
+                        print('\t\tUsing feature(s) {} and {{{}}} : accuracy is {:.1f}%'.format(current_set, k+1, accuracy*100))
                     if accuracy > best:
                         best = accuracy
                         feature_to_add = k
             current_set.add(feature_to_add)
             correspond[frozenset(current_set)] = best
             accuracies.append((feature_to_add+1, best))
-            #print('Added feature {} on level {}'.format(feature_to_add+1, i+1))
+            print('\nFeature(s) {} was best, with accuracy {:.1f}%\n'.format({s+1 for s in current_set}, best*100))
+        sorted_corr = sorted(correspond.items(), key=lambda x: x[1], reverse=True)
+        best_set, best_acc = sorted_corr[0]
+        print('The best feature subset is {}, which has an accuracy of {:.1f}%'.format({s+1 for s in best_set}, best_acc*100))
         return accuracies, correspond, Y, X
 
 
 if __name__ == '__main__':
+    print('Welcome to Chris\' feature selection!')
     filename = input('Enter file name:')
-    choice = input('Is this a test? ')
+    print('Which algorithm would you like to run? \n')
+    print('1) Forward Search')
+    print('2) Backward Search')
+    print('3) Chris\' Special Search')
+    choice = input()
     data = np.genfromtxt(filename)
     (m, n) = data.shape
     removal_size = int(m/10)
-    if choice == 'y':
+    search = featureSearch(data)
+    if choice == '1':
+        print('Starting forward feature search. . .')
+        print('\n\n This dataset has {} features and {} examples.'.format(n, m))
+        faccuracies, fcorr, Y, X = search.forward_feature_search()
+        print('Forward feature search finished.')
+    elif choice == '2':
+        print('Starting backward feature search. . .')
+        baccuracies, bcorr = search.backward_feature_search()
+        print('Backward feature search finished.')
+    elif choice == '3':
         i = 1
         while len(data) > 0:
             search = featureSearch(data)
             faccuracies, fcorr, Y, X = search.forward_feature_search()
             baccuracies, bcorr = search.backward_feature_search()
-            print('forward accuracies', faccuracies)
-            print('backward accuracies', baccuracies)
-            forward_graph = featGraph(faccuracies)
-            backward_graph = featGraph(baccuracies)
-            forward_graph.make_bars('forward', 'Forward Feature Selection run {} on {}'.format(i, filename))
-            backward_graph.make_bars('backward', 'Backward Feature Selection run {} on {}'.format(i, filename), 1)
-            i += 1
             new_data = sampleTraining(data, m, removal_size)
             data = new_data.remove_some_data()
             (m, n) = data.shape
-    else:
-        search = featureSearch(data)
-        faccuracies, fcorr, Y, X = search.forward_feature_search()
-        #baccuracies, bcorr = search.backward_feature_search()
-        print('forward accuracies', faccuracies)
-        #print('backward accuracies', baccuracies)
-        forward_graph = featGraph(faccuracies)
-        #backward_graph = featGraph(baccuracies)
-        forward_graph.make_bars('forward', 'Forward Feature Selection on {}'.format(filename))
-        #backward_graph.make_bars('backward', 'Backward Feature Selection on {}'.format(filename), 1)
-
-    forward_scatter = featGraph(faccuracies)
-    forward_scatter.make_scatter(fcorr, 'forward', '{}: Using only Features'.format(filename), Y, X)
-    forward_scatter.make_3d_scatter(fcorr, 'forward', '{}: Using only Features'.format(filename), Y, X)
